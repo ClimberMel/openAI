fa91539276563dfb051056d6c7ba694079b4936b
ClimberMel <climbermel@gmail.com>
2023-12-19 10:57:36 -0800

    Modify process to return response instead of print

--- code/ai_script.py
+++ code/ai_script.py
@@ -3,13 +3,11 @@ import time
 
 import code.file as file
 
-
 # Get my API key
 parms = file.read_json("parms/openAI.json")
 
 def chat_gpt(prompt):
     client = OpenAI(
-        # defaults to os.environ.get("OPENAI_API_KEY")
         api_key=parms["api_key"],
         )
 
@@ -18,9 +16,3 @@ def chat_gpt(prompt):
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content.strip()
-    #        print(response['choices'][0]['message']['content'])
-
-    #prompt = "Why are oil prices falling?"
-    #response = chat_gpt(prompt)
-
-    #return response
